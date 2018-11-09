a461ce07569fcf6b79091080cd2c290c728ba51c
jjmonte20 <jjmonte20@gmail.com>
2018-11-09 09:30:05 -0600

    made the limit 5 lengths for other search queries

--- requestHandler.js
+++ requestHandler.js
@@ -7,8 +7,6 @@
 $(document.body).on("click", ".btn",function(){
 
     var userInput = $("#userInput").val().trim();
-    // var usrInp = userInput.replace(/ /gi, "+");
-    // var btnInput = $(this).attr("data-name");
     var usrInp = userInput.replace(/ /gi, "+");
 
     console.log(userInput);
@@ -23,12 +21,10 @@ $(document.body).on("click", ".btn",function(){
             //storing response.results into a variable to make it easier on myself
             var myResults = response.results;
 
+            //cutting the results to 5
             myResults.length = 5;
 
         for (var i = 0; i < myResults.length; i++){
-
-            // Only take action if the result is less than 4
-            // if (myResults[i] < 4) {
 
             //create a div
             var rDiv = $("<div>");
@@ -44,8 +40,7 @@ $(document.body).on("click", ".btn",function(){
 
             // append so that the first items on the list show first
             $(".resultsHere").prepend(rDiv);
-            // console.log(myResults[i].formatted_address);
-            // }
+            
         }
         console.log(myResults);
     });
@@ -53,72 +48,162 @@ $(document.body).on("click", ".btn",function(){
 
     //These work, please uncomment 1 additional one to test that the results can show on multiple cards, then uncomment all when we know it works on all of them
 
-    // var userInput = $("#userInput").val().trim();
-    // // var usrInp = userInput.replace(/ /gi, "+");
-    // // var btnInput = $(this).attr("data-name");
-    // var usrInp = userInput.replace(/ /gi, "+");
+    var userInput = $("#userInput").val().trim();
+    var usrInp = userInput.replace(/ /gi, "+");
 
-    // console.log(userInput);
-    // console.log(usrInp);
-    // var queryUrl = "https://cors-ut-bootcamp.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?input=hospitals+near+" + usrInp + "&inputtype=textquery&radius=5000&" + "&key=AIzaSyCov3eDZmkrwgdqlChL-1PJVDcwaQfpTBc";
-    // console.log(queryUrl);
+    console.log(userInput);
+    console.log(usrInp);
+    var queryUrl = "https://cors-ut-bootcamp.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?input=hospitals+near+" + usrInp + "&inputtype=textquery&radius=5000&" + "&key=AIzaSyCov3eDZmkrwgdqlChL-1PJVDcwaQfpTBc";
+    console.log(queryUrl);
 
-    // $.ajax({
-    //     url: queryUrl,
-    //     method: "GET"
-    // }).then(function(response) {
-    //     console.log(response);
-    // });
-    // var userInput = $("#userInput").val().trim();
-    // // var usrInp = userInput.replace(/ /gi, "+");
-    // // var btnInput = $(this).attr("data-name");
-    // var usrInp = userInput.replace(/ /gi, "+");
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function(response) {
 
-    // console.log(userInput);
-    // console.log(usrInp);
-    // var queryUrl = "https://cors-ut-bootcamp.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?input=schools+near+" + usrInp + "&inputtype=textquery&radius=5000&" + "&key=AIzaSyCov3eDZmkrwgdqlChL-1PJVDcwaQfpTBc";
-    // console.log(queryUrl);
+        var myResults = response.results;
 
-    // $.ajax({
-    //     url: queryUrl,
-    //     method: "GET"
-    // }).then(function(response) {
-    //     console.log(response);
-    // });
-    // var userInput = $("#userInput").val().trim();
-    // // var usrInp = userInput.replace(/ /gi, "+");
-    // // var btnInput = $(this).attr("data-name");
-    // var usrInp = userInput.replace(/ /gi, "+");
+        myResults.length = 5
 
-    // console.log(userInput);
-    // console.log(usrInp);
-    // var queryUrl = "https://cors-ut-bootcamp.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?input=grocery+stores+near+" + usrInp + "&inputtype=textquery&radius=5000&" + "&key=AIzaSyCov3eDZmkrwgdqlChL-1PJVDcwaQfpTBc";
-    // console.log(queryUrl);
+        for (var i = 0; i < myResults.length; i++){
 
-    // $.ajax({
-    //     url: queryUrl,
-    //     method: "GET"
-    // }).then(function(response) {
-    //     console.log(response);
-    // });
-    // var userInput = $("#userInput").val().trim();
-    // // var usrInp = userInput.replace(/ /gi, "+");
-    // // var btnInput = $(this).attr("data-name");
-    // var usrInp = userInput.replace(/ /gi, "+");
+            //create a div
+            var rDiv = $("<div>");
 
-    // console.log(userInput);
-    // console.log(usrInp);
-    // var queryUrl = "https://cors-ut-bootcamp.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?input=post+offices+near+" + usrInp + "&inputtype=textquery&radius=5000&" + "&key=AIzaSyCov3eDZmkrwgdqlChL-1PJVDcwaQfpTBc";
-    // console.log(queryUrl);
+            //store div address
+            var address = myResults[i].formatted_address;
 
-    // $.ajax({
-    //     url: queryUrl,
-    //     method: "GET"
-    // }).then(function(response) {
-    //     console.log(response);
-    // });
+            // create text for the result item's address
+            var p = $("<p>").text("Address: " + address);
 
-    
+            // append the paragraph
+            rDiv.append(p);
+
+            // append so that the first items on the list show first
+            $(".resultsHere").prepend(rDiv);
+           
+        }
+        console.log(myResults);
+    });
+
+
+    var userInput = $("#userInput").val().trim();
+    var usrInp = userInput.replace(/ /gi, "+");
+
+    console.log(userInput);
+    console.log(usrInp);
+    var queryUrl = "https://cors-ut-bootcamp.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?input=schools+near+" + usrInp + "&inputtype=textquery&radius=5000&" + "&key=AIzaSyCov3eDZmkrwgdqlChL-1PJVDcwaQfpTBc";
+    console.log(queryUrl);
+
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function(response) {
+        
+        var myResults = response.results;
+
+        myResults.length = 5
+
+        for (var i = 0; i < myResults.length; i++){
+
+            //create a div
+            var rDiv = $("<div>");
+
+            //store div address
+            var address = myResults[i].formatted_address;
+
+            // create text for the result item's address
+            var p = $("<p>").text("Address: " + address);
+
+            // append the paragraph
+            rDiv.append(p);
+
+            // append so that the first items on the list show first
+            $(".resultsHere").prepend(rDiv);
+           
+        }
+        console.log(myResults);
+    });
+
+
+    var userInput = $("#userInput").val().trim(); 
+    var usrInp = userInput.replace(/ /gi, "+");
+
+    console.log(userInput);
+    console.log(usrInp);
+    var queryUrl = "https://cors-ut-bootcamp.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?input=grocery+stores+near+" + usrInp + "&inputtype=textquery&radius=5000&" + "&key=AIzaSyCov3eDZmkrwgdqlChL-1PJVDcwaQfpTBc";
+    console.log(queryUrl);
+
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function(response) {
+
+        var myResults = response.results;
+
+        myResults.length = 5
+
+        for (var i = 0; i < myResults.length; i++){
+
+            //create a div
+            var rDiv = $("<div>");
+
+            //store div address
+            var address = myResults[i].formatted_address;
+
+            // create text for the result item's address
+            var p = $("<p>").text("Address: " + address);
+
+            // append the paragraph
+            rDiv.append(p);
+
+            // append so that the first items on the list show first
+            $(".resultsHere").prepend(rDiv);
+           
+        }
+        console.log(myResults);
+    });
+
+
+    var userInput = $("#userInput").val().trim();
+    var usrInp = userInput.replace(/ /gi, "+");
+
+    console.log(userInput);
+    console.log(usrInp);
+    var queryUrl = "https://cors-ut-bootcamp.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?input=post+offices+near+" + usrInp + "&inputtype=textquery&radius=5000&" + "&key=AIzaSyCov3eDZmkrwgdqlChL-1PJVDcwaQfpTBc";
+    console.log(queryUrl);
+
+    $.ajax({
+        url: queryUrl,
+        method: "GET"
+    }).then(function(response) {
+        
+        var myResults = response.results;
+
+        myResults.length = 5
+
+        for (var i = 0; i < myResults.length; i++){
+
+            //create a div
+            var rDiv = $("<div>");
+
+            //store div address
+            var address = myResults[i].formatted_address;
+
+            // create text for the result item's address
+            var p = $("<p>").text("Address: " + address);
+
+            // append the paragraph
+            rDiv.append(p);
+
+            // append so that the first items on the list show first
+            $(".resultsHere").prepend(rDiv);
+           
+        }
+        console.log(myResults);
+    });
+
+// these are the closers for the on click function, do not touch   
 });
 
 // This value should be whatever the user types, best practice would be to be as specific as possible, but we need 
