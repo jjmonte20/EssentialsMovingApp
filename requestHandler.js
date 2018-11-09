724eeaeb303e1f60f297db443eb22da1f4f81570
jjmonte20 <jjmonte20@gmail.com>
2018-11-09 08:59:38 -0600

    added ability for results to display on screen, limit to 5 results

--- requestHandler.js
+++ requestHandler.js
@@ -20,72 +20,103 @@ $(document.body).on("click", ".btn",function(){
         url: queryUrl,
         method: "GET"
     }).then(function(response) {
-        console.log(response);
+            //storing response.results into a variable to make it easier on myself
+            var myResults = response.results;
+
+            myResults.length = 5;
+
+        for (var i = 0; i < myResults.length; i++){
+
+            // Only take action if the result is less than 4
+            // if (myResults[i] < 4) {
+
+            //create a div
+            var rDiv = $("<div>");
+
+            //store div address
+            var address = myResults[i].formatted_address;
+
+            // create text for the result item's address
+            var p = $("<p>").text("Address: " + address);
+
+            // append the paragraph
+            rDiv.append(p);
+
+            // append so that the first items on the list show first
+            $(".resultsHere").prepend(rDiv);
+            // console.log(myResults[i].formatted_address);
+            // }
+        }
+        console.log(myResults);
     });
-    var userInput = $("#userInput").val().trim();
+
+
+    //These work, please uncomment 1 additional one to test that the results can show on multiple cards, then uncomment all when we know it works on all of them
+
+    // var userInput = $("#userInput").val().trim();
+    // // var usrInp = userInput.replace(/ /gi, "+");
+    // // var btnInput = $(this).attr("data-name");
     // var usrInp = userInput.replace(/ /gi, "+");
-    // var btnInput = $(this).attr("data-name");
-    var usrInp = userInput.replace(/ /gi, "+");
 
-    console.log(userInput);
-    console.log(usrInp);
-    var queryUrl = "https://cors-ut-bootcamp.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?input=hospitals+near+" + usrInp + "&inputtype=textquery&radius=5000&" + "&key=AIzaSyCov3eDZmkrwgdqlChL-1PJVDcwaQfpTBc";
-    console.log(queryUrl);
+    // console.log(userInput);
+    // console.log(usrInp);
+    // var queryUrl = "https://cors-ut-bootcamp.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?input=hospitals+near+" + usrInp + "&inputtype=textquery&radius=5000&" + "&key=AIzaSyCov3eDZmkrwgdqlChL-1PJVDcwaQfpTBc";
+    // console.log(queryUrl);
 
-    $.ajax({
-        url: queryUrl,
-        method: "GET"
-    }).then(function(response) {
-        console.log(response);
-    });
-    var userInput = $("#userInput").val().trim();
+    // $.ajax({
+    //     url: queryUrl,
+    //     method: "GET"
+    // }).then(function(response) {
+    //     console.log(response);
+    // });
+    // var userInput = $("#userInput").val().trim();
+    // // var usrInp = userInput.replace(/ /gi, "+");
+    // // var btnInput = $(this).attr("data-name");
     // var usrInp = userInput.replace(/ /gi, "+");
-    // var btnInput = $(this).attr("data-name");
-    var usrInp = userInput.replace(/ /gi, "+");
 
-    console.log(userInput);
-    console.log(usrInp);
-    var queryUrl = "https://cors-ut-bootcamp.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?input=schools+near+" + usrInp + "&inputtype=textquery&radius=5000&" + "&key=AIzaSyCov3eDZmkrwgdqlChL-1PJVDcwaQfpTBc";
-    console.log(queryUrl);
+    // console.log(userInput);
+    // console.log(usrInp);
+    // var queryUrl = "https://cors-ut-bootcamp.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?input=schools+near+" + usrInp + "&inputtype=textquery&radius=5000&" + "&key=AIzaSyCov3eDZmkrwgdqlChL-1PJVDcwaQfpTBc";
+    // console.log(queryUrl);
 
-    $.ajax({
-        url: queryUrl,
-        method: "GET"
-    }).then(function(response) {
-        console.log(response);
-    });
-    var userInput = $("#userInput").val().trim();
+    // $.ajax({
+    //     url: queryUrl,
+    //     method: "GET"
+    // }).then(function(response) {
+    //     console.log(response);
+    // });
+    // var userInput = $("#userInput").val().trim();
+    // // var usrInp = userInput.replace(/ /gi, "+");
+    // // var btnInput = $(this).attr("data-name");
     // var usrInp = userInput.replace(/ /gi, "+");
-    // var btnInput = $(this).attr("data-name");
-    var usrInp = userInput.replace(/ /gi, "+");
 
-    console.log(userInput);
-    console.log(usrInp);
-    var queryUrl = "https://cors-ut-bootcamp.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?input=grocery+stores+near+" + usrInp + "&inputtype=textquery&radius=5000&" + "&key=AIzaSyCov3eDZmkrwgdqlChL-1PJVDcwaQfpTBc";
-    console.log(queryUrl);
+    // console.log(userInput);
+    // console.log(usrInp);
+    // var queryUrl = "https://cors-ut-bootcamp.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?input=grocery+stores+near+" + usrInp + "&inputtype=textquery&radius=5000&" + "&key=AIzaSyCov3eDZmkrwgdqlChL-1PJVDcwaQfpTBc";
+    // console.log(queryUrl);
 
-    $.ajax({
-        url: queryUrl,
-        method: "GET"
-    }).then(function(response) {
-        console.log(response);
-    });
-    var userInput = $("#userInput").val().trim();
+    // $.ajax({
+    //     url: queryUrl,
+    //     method: "GET"
+    // }).then(function(response) {
+    //     console.log(response);
+    // });
+    // var userInput = $("#userInput").val().trim();
+    // // var usrInp = userInput.replace(/ /gi, "+");
+    // // var btnInput = $(this).attr("data-name");
     // var usrInp = userInput.replace(/ /gi, "+");
-    // var btnInput = $(this).attr("data-name");
-    var usrInp = userInput.replace(/ /gi, "+");
 
-    console.log(userInput);
-    console.log(usrInp);
-    var queryUrl = "https://cors-ut-bootcamp.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?input=post+offices+near+" + usrInp + "&inputtype=textquery&radius=5000&" + "&key=AIzaSyCov3eDZmkrwgdqlChL-1PJVDcwaQfpTBc";
-    console.log(queryUrl);
+    // console.log(userInput);
+    // console.log(usrInp);
+    // var queryUrl = "https://cors-ut-bootcamp.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?input=post+offices+near+" + usrInp + "&inputtype=textquery&radius=5000&" + "&key=AIzaSyCov3eDZmkrwgdqlChL-1PJVDcwaQfpTBc";
+    // console.log(queryUrl);
 
-    $.ajax({
-        url: queryUrl,
-        method: "GET"
-    }).then(function(response) {
-        console.log(response);
-    });
+    // $.ajax({
+    //     url: queryUrl,
+    //     method: "GET"
+    // }).then(function(response) {
+    //     console.log(response);
+    // });
 
     
 });
